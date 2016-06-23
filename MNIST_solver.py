f310f92da9101856d210f243107bb1c8923a3805
Idan Schwartz <idanschwartz@gmail.com>
2016-06-23 13:58:14 +0300

    ds

--- MNIST_solver.py
+++ MNIST_solver.py
@@ -26,7 +26,7 @@ with tf.Session() as sess:
 
     #feed data batch
     for i in range(1000):
-        batch_xs, batch_ys = mnist.train.next_batch(100)
+        batch_xs, batch_ys = mnist.train.next_batch(50)
         sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
