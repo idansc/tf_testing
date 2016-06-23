ace20197fbdf9cb3321874c5504e1df6974f5ea9
Idan Schwartz <idanschwartz@gmail.com>
2016-06-23 13:40:44 +0300

    interactive

--- MNIST_solver.py
+++ MNIST_solver.py
@@ -5,29 +5,29 @@ import tensorflow as tf
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-with tf.Session() as sess:
-    #placeholfers. x - for iamge. y - output class. y consist a 2d tensor, each row is a one-hot 10-dim vectore.
-    #shape is optional - allow to catch bugs.
-    x = tf.placeholder(tf.float32, shape=[None, 784])
-    y_ = tf.placeholder(tf.float32, shape=[None, 10])
-    #the weights
-    W = tf.Variable(tf.zeros([784, 10]))
-    b = tf.Variable(tf.zeros([10]))
-    #init
-    sess.run(tf.initialize_all_variables())
-    #apply softmax
-    y = tf.nn.softmax(tf.matmul(W,x)+b)
+sess = tf.InteractiveSession()
+#placeholfers. x - for iamge. y - output class. y consist a 2d tensor, each row is a one-hot 10-dim vectore.
+#shape is optional - allow to catch bugs.
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+#the weights
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+#init
+sess.run(tf.initialize_all_variables())
+#apply softmax
+y = tf.nn.softmax(tf.matmul(W,x)+b)
 
-    #CE loss
-    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+#CE loss
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
-    #GD
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+#GD
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-    #feed data batch
-    for i in range(1000):
-        batch = mnist.train.next_batch(50)
-        train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+#feed data batch
+for i in range(1000):
+    batch = mnist.train.next_batch(50)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
